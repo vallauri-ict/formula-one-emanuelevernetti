ca618fd78741c69e707fce682bf484dcfc4c8b38
Emanuele Vernetti <e.vernetti.1033@vallauri.edu>
2020-10-07 21:36:00 +0200

    Add CLI commands to create Drivers and Teams Tables

--- FormulaOneConsole/Program.cs
+++ FormulaOneConsole/Program.cs
@@ -2,18 +2,14 @@
 using System.Data.SqlClient;
 using System.IO;
 
-namespace FormulaOneConsole
-{
-    class Program
-    {
+namespace FormulaOneConsole {
+    class Program {
         public const string WORKINGPATH = @"C:\data\formulaone\";
         private const string CONNECTION_STRING = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFileName=" + WORKINGPATH + @"FormulaOne.mdf;Integrated Security=True";
-        static void Main(string[] args)
-        {
+        static void Main(string[] args) {
             Console.WriteLine("*** FORMULA ONE - BATCH OPERATIONS");
             char scelta = ' ';
-            do
-            {
+            do {
                 Console.WriteLine("1 - Create Countries");
                 Console.WriteLine("2 - Create Teams");
                 Console.WriteLine("3 - Create Drivers");
@@ -22,19 +18,24 @@ namespace FormulaOneConsole
                 Console.WriteLine("---------------");
                 Console.Write("\nScelta desiderata: ");
                 scelta = Convert.ToChar(Console.ReadLine());
-                switch (scelta)
-                {
-                    case '1':
-                        {
+                switch (scelta) {
+                    case '1': {
                             ExecuteSqlScript("Countries.sql");
+                            break;
+                        }
+                    case '2': {
+                            ExecuteSqlScript("Teams.sql");
+                            break;
+                        }
+                    case '3': {
+                            ExecuteSqlScript("Drivers.sql");
                             break;
                         }
                 }
             } while (scelta != 'X' && scelta != 'x');
         }
 
-        private static void ExecuteSqlScript(string sqlScriptName)
-        {
+        private static void ExecuteSqlScript(string sqlScriptName) {
             var fileContent = File.ReadAllText(WORKINGPATH + sqlScriptName);
             fileContent = fileContent.Replace("\r", "");
             fileContent = fileContent.Replace("\n", "");
@@ -43,32 +44,26 @@ namespace FormulaOneConsole
 
             SqlConnection con = new SqlConnection(CONNECTION_STRING);
 
-            using (con)
-            {
+            using (con) {
                 SqlCommand cmd = new SqlCommand("query", con);
                 con.Open();
                 int i = 0;
                 int nErr = 0;
-                foreach (var query in sqlqueries)
-                {
+                foreach (var query in sqlqueries) {
                     cmd.CommandText = query;
                     i++;
-                    try
-                    {
+                    try {
                         cmd.ExecuteNonQuery();
                     }
-                    catch (SqlException ex)
-                    {
-                        Console.WriteLine("Errore durante l'esecuzione della query "+i+"\n"+ex.Message);
+                    catch (SqlException ex) {
+                        Console.WriteLine("Errore durante l'esecuzione della query " + i + "\n" + ex.Message);
                         nErr++;
                     }
                 }
-                if (nErr!=0)
-                {
+                if (nErr != 0) {
                     Console.WriteLine("Si sono verificati " + nErr + " errori");
                 }
-                else
-                {
+                else {
                     Console.Write("\nEsecuzione effettuata correttamente");
                 }
             }
