3d24ac9af2be0d6de7aa7c4b85be06e1e75727df
Emanuele Vernetti <e.vernetti.1033@vallauri.edu>
2020-10-07 18:54:42 +0200

    Aggiunti file SQL relativi a drivers e teams

---
+++ Data/Drivers.sql
@@ -0,0 +1,233 @@
+CREATE TABLE [dbo].[Driver]
+(
+	[ID] int PRIMARY KEY IDENTITY(1,1),
+	[Number] int UNIQUE NOT NULL,
+	[Name] varchar(255) NOT NULL,
+	[DOB] date,
+	[HelmetImage] image NOT NULL,
+	[Image] image NOT NULL,
+	[TeamID] int NOT NULL,
+	[Podiums] int NOT NULL,
+	[CountryCode] char(2) NOT NULL,
+);
+
+INSERT INTO [Driver]
+VALUES(
+		44,
+		'Lewis Hamilton',
+        CONVERT(date,'1985-01-07')
+		'C:\data\formulaone\img\lewisHamilton-Helmet.jpg',
+		'C:\data\formulaone\img\lewisHamilton.jpg',
+		1,
+		159,
+		'UK',        
+);
+INSERT INTO [Driver]
+VALUES(
+		77,
+		'Valtteri Bottas',
+        CONVERT(date,'1989-08-28')
+		'C:\data\formulaone\img\valterriBottas-Helmet.jpg',
+		'C:\data\formulaone\img\valterriBottas.jpg',
+		1,
+		53,
+		'FI',        
+);
+INSERT INTO [Driver]
+VALUES(
+		33,
+		'Max Verstappen',
+        CONVERT(date,'1997-09-30')
+		'C:\data\formulaone\img\maxVerstappen-Helmet.jpg',
+		'C:\data\formulaone\img\maxVerstappen.jpg',
+		2,
+		38,
+		'NL',        
+);
+INSERT INTO [Driver]
+VALUES(
+		4,
+		'Lando Norris',
+        CONVERT(date,'1999-11-13')
+		'C:\data\formulaone\img\landoNorris-Helmet.jpg',
+		'C:\data\formulaone\img\landoNorris.jpg',
+		3,
+		1,
+		'UK',        
+);
+INSERT INTO [Driver]
+VALUES(
+		23,
+		'Alexander Albon',
+        CONVERT(date,'1999-03-23')
+		'C:\data\formulaone\img\alexanderAlbon-Helmet.jpg',
+		'C:\data\formulaone\img\alexanderAlbon.jpg',
+		2,
+		1,
+		'TH',        
+);
+INSERT INTO [Driver]
+VALUES(
+		3,
+		'Daniel Ricciardo',
+        CONVERT(date,'1989-07-01')
+		'C:\data\formulaone\img\danielRicciardo-Helmet.jpg',
+		'C:\data\formulaone\img\danielRicciardo.jpg',
+		5,
+		29,
+		'AU',        
+);
+INSERT INTO [Driver]
+VALUES(
+		16,
+		'Charles Leclerc',
+        CONVERT(date,'1997-10-16')
+		'C:\data\formulaone\img\charlesLeclerc-Helmet.jpg',
+		'C:\data\formulaone\img\charlesLeclerc.jpg',
+		6,
+		12,
+		'PL',        
+);
+INSERT INTO [Driver]
+VALUES(
+		18,
+		'Lance Stroll',
+        CONVERT(date,'1998-10-29')
+		'C:\data\formulaone\img\lanceStroll-Helmet.jpg',
+		'C:\data\formulaone\img\lanceStroll.jpg',
+		4,
+		2,
+		'CA',        
+);
+INSERT INTO [Driver]
+VALUES(
+		11,
+		'Sergio Perez',
+        CONVERT(date,'1990-01-26')
+		'C:\data\formulaone\img\sergioPerez-Helmet.jpg',
+		'C:\data\formulaone\img\sergioPerez.jpg',
+		4,
+		8,
+		'MX',        
+);
+INSERT INTO [Driver]
+VALUES(
+		10,
+		'Pierre Gasly',
+        CONVERT(date,'1996-02-07')
+		'C:\data\formulaone\img\pierreGasly-Helmet.jpg',
+		'C:\data\formulaone\img\pierreGasly.jpg',
+		7,
+		2,
+		'FR',        
+);
+INSERT INTO [Driver]
+VALUES(
+		55,
+		'Carlos Sainz',
+        CONVERT(date,'1994-09-01')
+		'C:\data\formulaone\img\carlosSainz-Helmet.jpg',
+		'C:\data\formulaone\img\carlosSainz.jpg',
+		3,
+		2,
+		'ES',        
+);
+INSERT INTO [Driver]
+VALUES(
+		31,
+		'Esteban Ocon',
+        CONVERT(date,'1996-09-17')
+		'C:\data\formulaone\img\estebanOcon-Helmet.jpg',
+		'C:\data\formulaone\img\estebanOcon.jpg',
+		5,
+		0,
+		'FR',        
+);
+INSERT INTO [Driver]
+VALUES(
+		5,
+		'Sebastian Vettel',
+        CONVERT(date,'1987-07-03')
+		'C:\data\formulaone\img\sebastianVettel-Helmet.jpg',
+		'C:\data\formulaone\img\sebastianVettel.jpg',
+		6,
+		120,
+		'DE',        
+);
+INSERT INTO [Driver]
+VALUES(
+		26,
+		'Daniil Kvyat',
+        CONVERT(date,'1994-04-26')
+		'C:\data\formulaone\img\daniilKvyat-Helmet.jpg',
+		'C:\data\formulaone\img\daniilKvyat.jpg',
+		7,
+		3,
+		'RU',        
+);
+INSERT INTO [Driver]
+VALUES(
+		7,
+		'Kimi Räikkönen',
+        CONVERT(date,'1979-10-17')
+		'C:\data\formulaone\img\kimiRaikkonen-Helmet.jpg',
+		'C:\data\formulaone\img\kimiRaikkonen.jpg',
+		8,
+		103,
+		'FI',        
+);
+INSERT INTO [Driver]
+VALUES(
+		99,
+		'Antonio Giovinazzi',
+        CONVERT(date,'1993-12-14')
+		'C:\data\formulaone\img\antonioGiovinazzi-Helmet.jpg',
+		'C:\data\formulaone\img\antonioGiovinazzi.jpg',
+		8,
+		0,
+		'IT',        
+);
+INSERT INTO [Driver]
+VALUES(
+		20,
+		'Kevin Magnussen',
+        CONVERT(date,'1992-10-05')
+		'C:\data\formulaone\img\kevinMagnussen-Helmet.jpg',
+		'C:\data\formulaone\img\kevinMagnussen.jpg',
+		9,
+		1,
+		'DK',        
+);
+INSERT INTO [Driver]
+VALUES(
+		6,
+		'Nicholas Latifi',
+        CONVERT(date,'1995-06-29')
+		'C:\data\formulaone\img\nicholasLatifi-Helmet.jpg',
+		'C:\data\formulaone\img\nicholasLatifi.jpg',
+		10,
+		0,
+		'CA',        
+);
+INSERT INTO [Driver]
+VALUES(
+		63,
+		'George Russell',
+        CONVERT(date,'1998-02-15')
+		'C:\data\formulaone\img\georgeRussell-Helmet.jpg',
+		'C:\data\formulaone\img\georgeRussell.jpg',
+		10,
+		0,
+		'UK',        
+);
+INSERT INTO [Driver]
+VALUES(
+		8,
+		'Romain Grosjean',
+        CONVERT(date,'1986-04-17')
+		'C:\data\formulaone\img\romainGrosjean-Helmet.jpg',
+		'C:\data\formulaone\img\romainGrosjean.jpg',
+		9,
+		10,
+		'FR',        
+);
---
+++ Data/Teams.sql
@@ -0,0 +1,146 @@
+CREATE TABLE [dbo].[Team]
+(
+	[ID] int PRIMARY KEY IDENTITY(1,1),
+    [TeamName] varchar(255) UNIQUE NOT NULL,
+    [TeamLogo] image NOT NULL,
+    [Base] varchar(255) NOT NULL,
+    [TeamChief] varchar(255) NOT NULL,
+    [TechnicalChief] varchar(255) NOT NULL,
+	[PowerUnit] varchar(255) NOT NULL,
+	[CarImage] image NOT NULL,
+	[CountryID] char(2) NOT NULL,
+	[WorldChampionships] int default 0,
+    [PolePositions] int default 0
+);
+
+INSERT INTO [Team]
+VALUES(
+		'Mercedes-AMG Petronas F1 Team',
+		'C:\data\formulaone\img\mercedesTeamLogo.jpg',
+        'Brackley',
+		'Toto Wolff',
+		'James Allison',
+		'Mercedes',
+		'C:\data\formulaone\img\mercedesCarImage.jpg',
+		'UK',
+		6,
+		113        
+);
+INSERT INTO [Team]
+VALUES(
+		'Aston Martin Red Bull Racing',
+		'C:\data\formulaone\img\redBullTeamLogo.jpg',
+        'Milton Keynes',
+		'Christian Horner',
+		'Pierre Waché',
+		'Honda',
+		'C:\data\formulaone\img\redBullCarImage.jpg',
+		'UK',
+		4,
+		66        
+);
+INSERT INTO [Team]
+VALUES(
+		'McLaren F1 Team',
+		'C:\data\formulaone\img\mcLarenTeamLogo.jpg',
+        'Woking',
+		'Andreas Seidl',
+		'James Key',
+		'Renault',
+		'C:\data\formulaone\img\mcLarenCarImage.jpg',
+		'UK',
+		8,
+		155        
+);
+INSERT INTO [Team]
+VALUES(
+		'BWT Racing Point F1 Team',
+		'C:\data\formulaone\img\racingPointTeamLogo.jpg',
+        'Silverstone',
+		'Otmar Szafnauer',
+		'Andrew Green',
+		'BWT Mercedes',
+		'C:\data\formulaone\img\racingPointCarImage.jpg',
+		'UK',
+		0,
+		0        
+);
+INSERT INTO [Team]
+VALUES(
+		'Renault DP World F1 Team',
+		'C:\data\formulaone\img\renaultTeamLogo.jpg',
+        'Enstone',
+		'Cyril Abiteboul',
+		'TBC',
+		'Renault',
+		'C:\data\formulaone\img\renaultCarImage.jpg',
+		'UK',
+		2,
+		20        
+);
+INSERT INTO [Team]
+VALUES(
+		'Scuderia Ferrari Mission Winnow',
+		'C:\data\formulaone\img\ferrariTeamLogo.jpg',
+        'Maranello',
+		'Mattia Binotto',
+		'Simone Resta',
+		'Ferrari',
+		'C:\data\formulaone\img\ferrariCarImage.jpg',
+		'IT',
+		16,
+		221        
+);
+INSERT INTO [Team]
+VALUES(
+		'Scuderia AlphaTauri Honda',
+		'C:\data\formulaone\img\alphaTauriTeamLogo.jpg',
+        'Faenza',
+		'Franz Tost',
+		'Jody Egginton',
+		'Honda',
+		'C:\data\formulaone\img\alphaTauriCarImage.jpg',
+		'IT',
+		0,
+		1        
+);
+INSERT INTO [Team]
+VALUES(
+		'Alfa Romeo Racing ORLEN',
+		'C:\data\formulaone\img\alfaRomeoTeamLogo.jpg',
+        'Hinwil',
+		'Frédéric Vasseur',
+		'Jan Monchaux',
+		'Ferrari',
+		'C:\data\formulaone\img\alfaRomeoCarImage.jpg',
+		'IT',
+		0,
+		1        
+);
+INSERT INTO [Team]
+VALUES(
+		'Haas F1 Team',
+		'C:\data\formulaone\img\haasTeamLogo.jpg',
+        'Kannapolis',
+		'Guenther Steiner',
+		'Rob Taylor',
+		'Ferrari',
+		'C:\data\formulaone\img\haasCarImage.jpg',
+		'US',
+		0,
+		0        
+);
+INSERT INTO [Team]
+VALUES(
+		'Williams Racing',
+		'C:\data\formulaone\img\williamsTeamLogo.jpg',
+        'Grove',
+		'Simon Roberts',
+		'TBC',
+		'Mercedes',
+		'C:\data\formulaone\img\williamsCarImage.jpg',
+		'UK',
+		9,
+		129        
+);
+
