448ea11674db08ee040e195735c06135fa4e412d
Emanuele Vernetti <58587228+emanuelevernetti@users.noreply.github.com>
2021-01-14 13:19:05 +0100

    Added API .Net Core app

---
+++ ToDoApi/Models/ToDoContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace ToDoApi.Models {
+    public class ToDoContext : DbContext {
+        public ToDoContext(DbContextOptions<ToDoContext> options)
+            : base(options) {
+        }
+
+        public DbSet<ToDoItem> TodoItems { get; set; }
+    }
+}
